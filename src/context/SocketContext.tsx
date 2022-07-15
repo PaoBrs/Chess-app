b075c11c89c5482fa1d1c86b11d800e01ce2c819
unknown <p.barbosa93@gmail.com>
2022-07-15 01:00:00 -0500

    modifying socket url

--- src/context/SocketContext.tsx
+++ src/context/SocketContext.tsx
@@ -6,10 +6,12 @@ type Props = {
   children: React.ReactNode,
 }
 
+const socketUrl = process.env.NODE_ENV === 'production' ?
+  'https://quiet-dusk-46720.herokuapp.com/' : 'http://localhost:8080'
+
 export const SocketProvider = ({ children }: Props) => {
 
-  // const { socket, online } = useSocket('https://quiet-dusk-46720.herokuapp.com/');
-  const { socket, online } = useSocket('http://localhost:8080');
+  const { socket, online } = useSocket(socketUrl)
 
   return (
     <SocketContext.Provider value={{ socket, online }
